bd0c10b830939f062f941f9e6d69111b4479a4d6
alisawera <alisawera@7951ee24-05a5-11df-8c27-7d37615f5189>
2010-01-26 15:12:57 +0000

    Created a full-fledged installation script

--- install.sh
+++ install.sh
@@ -1,20 +1,29 @@
 #!/bin/bash
 
-# Installing files
+echo This script asked you to let it operate as root.
+sudo su
+
+echo Package is downloading into the current directory...
+git svn clone -s http://pysatel.googlecode.com/svn
+nano pysatel.conf
+echo Database is configuring...
+DBNAME=`cat pysatel.conf | grep mysqldatabase | sed s/mysqldatabase// | sed s/=// |  awk '{ print $1 }'`
+USERNAME=`cat pysatel.conf | grep mysqluser | sed s/mysqluser// | sed s/=// |  awk '{ print $1 }'`
+PSW=`cat pysatel.conf | grep mysqlpassword | sed s/mysqlpassword// | sed s/=// |  awk '{ print $1 }'`
+echo Enter your root mysql password below.
+mysql --user=root -p --execute="create database $DBNAME; grant insert,select,create,drop on $DBNAME.* to $USERNAME identified by '$PSW'";
+
+echo Installing files...
 mv pysatel.conf /etc/
 mv pysatel-admin.py /usr/local/bin/
 mkdir -p /usr/local/lib/python2.5/site-packages/pysatel/telemetry
-mv coord.py export.py  process.py /usr/local/lib/python2.5/site-packages/pysatel/
+mv coord.py export.py process.py /usr/local/lib/python2.5/site-packages/pysatel/
 touch /usr/local/lib/python2.5/site-packages/pysatel/telemetry/__init__.py
 
-# Installing cron
+echo Installing cron...
 crontab -l > cron.tmp
 echo @hourly /usr/local/lib/python2.5/site-packages/pysatel/process.py >> cron.tmp
 cat cron.tmp | crontab -
 rm cron.tmp
-
-echo "1. Edit /etc/pysatel.conf to specify correct directories for data and TLE archives, and correct credentials to connect to MySQL with."
-echo "2. Log on to your MySQL RDBS and create the appropriate database and user. It should look like this:"
-echo "      create database pysatel;"
-echo "      grant insert,select,create,drop on pysatel.* to `pysatel` identified by '1234';"
-echo "Make sure the credentials you enter match those specified in the /etc/pysatel.conf file!"
+echo Installation completed successfully.
+exit # exit sudo su
