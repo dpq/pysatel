67a01371829a9c7a43b52aea75a47340cb0a5da4
jaffar.rumith@gmail.com <jaffar.rumith@gmail.com@7951ee24-05a5-11df-8c27-7d37615f5189>
2010-07-13 11:12:26 +0000

    Moving to replace SQL Alchemy. This commit is broken, do not use it!

--- trunk/dbdriver.py
+++ trunk/dbdriver.py
@@ -1,100 +1,97 @@
 #!/usr/bin/python
+"""PySatel - a Python framework for automated processing of scientific data
+   acquired from spacecraft instruments.
+   Copyright (C) 2010 David Parunakian
 
-""" 
-usage:
-db = Db("$TYPE", {"host" : , "db" : , "user" : , "passwd"})
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 
-import os
-import cx_Oracle
-import MySQLdb
-from MySQLdb import MySQLError
+from sqlalchemy import create_engine
+from sqlalchemy import Table, Column, MetaData, Index
+from sqlalchemy.types import DateTime, Integer
 
-class MyDict(dict):
-	def __missing__(self, key):
-		return key
 
-class Db:
-	def __init__(self, dbtype, params): # params is a dict
-		self.types = {
-			# ["datetime", "timestamp"] :(((
-			"oracle" : MyDict([["int", "number"], ["datetime", "date"]]),
-			"mysql"  : MyDict()
-		}
-		self.type = dbtype
-		if self.type == "oracle":
-			print 'self.conn = cx_Oracle.connect(' + "%s/%s@%s"%(params["user"], params["passwd"], params["tns"]) + ')'
-			self.conn = cx_Oracle.connect("%s/%s@%s"%(params["user"], params["passwd"], params["tns"]))
-			self.maxAllowedPacked = 1024*1024 # TODO
-		elif self.type == "mysql":
-			self.conn = MySQLdb.connect(host = params["host"], user = params["user"], passwd = params["passwd"], db = params["db"])
-		self.dbname = params["db"]
-		self.cursor = self.conn.cursor()
-		if self.type == "mysql":
-			self.cursor.execute("show variables;")
-			self.maxAllowedPacked = int(dict(self.cursor.fetchall())["max_allowed_packet"])
-
-	def createTable(self, table, cols, primarykey):
-		if self.type == "oracle":
-			# "colname" $TYPE other_comments, ...
-			cols = ",".join(map(lambda col : '"%s" %s '%(col, self.types[self.type][cols[col].pop("type")]) + " ".join(map(lambda cl: cl + " " + cols[col][cl],cols[col])), sorted(cols.keys())))
-			self.cursor.execute('CREATE TABLE "%s" (%s, PRIMARY KEY ("%s"))'%(table, cols, primarykey))
-		elif self.type == "mysql":
-			cols = ",".join(map(lambda col : "`%s` %s "%(col, self.types[self.type][cols[col].pop("type")]) + " ".join(map(lambda cl: cl + " " + cols[col][cl],cols[col])), sorted(cols.keys())))
-			self.cursor.execute('CREATE TABLE %s.`%s` (%s, PRIMARY KEY (`%s`));'%(self.dbname, table, cols, primarykey))
-
-	def dropTable(self, table):
-		if self.type == "oracle":
-			self.cursor.execute('DROP TABLE "%s"'%(table))
-		elif self.type == "mysql":
-			self.cursor.execute("DROP TABLE %s.`%s`;"%(self.dbname, table))
-
-	def insert(self, table, header, valueslists):
-		if len(valueslists) == 0:
-			print "KAKOGO??? Valueslists is empty :("
-			return
-		if self.type == "oracle":
-			columns = ",".join(map(lambda h : '"%s"'%h, header))
-			placeholders = ",".join(map(lambda i : ":" + str(i+1), range(len(header))))
-			request = 'INSERT INTO smdc."%s"('%table + columns + ') VALUES (to_date(:1, \'yyyy-mm-dd hh24:mi:ss\'),' + placeholders[3:] + ')'
-			self.cursor.prepare(request)
-			try:
-				self.cursor.executemany(None, valueslists)
-				self.conn.commit()
-			except cx_Oracle.Error, strerror:
-				print strerror[:-1]
-		elif self.type == "mysql":
-			columns = ",".join(header)
-			placeholders = ("%s,"*len(header))[:-1]
-			trying = 0
-			try:
-				maxRows = self.maxAllowedPacked / len(" ".join(map(lambda v : str(v), valueslists[0]))) / 2
-				i = 0
-				while i < len(valueslists):
-					self.cursor.executemany("insert into `" + table + "` ("+columns+") values ("+placeholders+")", valueslists[i:i+maxRows])
-					i += maxRows
-			except MySQLError, strerror:
-				print "MySQLError :", strerror, "trying ..."
-				trying = 1
-			if trying:
-				print "Trying row by row... (", len(valueslists), ") rows" 
-				for i in range(len(valueslists)):
-					try:
-#						print  "insert into `%s` (%s) values (%s)"%(table, columns, )
-#						print len(placeholders.split("s")), placeholders
-#						print len(valueslists[i])
-						print "insert into `" + table + "` ("+columns+") values ('%s',"%(valueslists[0]) + placeholders[:-2]%(",".join(valueslists[i][1:]))+");"
-						self.cursor.execute(("insert into `" + table + "` ("+columns+") values ("+placeholders+");")%valueslists[i])
-					except MySQLError, strerror:
-						print "MySQLError :", strerror
-#						1/0
-					except:
-						pass
-#			2/0
-		return 0
-
-	def __del__(self):
-		self.cursor.close()
-		self.conn.close()
-
+class Driver:
+    def __init__(self, dialect, args, driver=""): # params is a dict
+        if driver != "":
+            dialect = "%s+%s" % (dialect, driver)
+        self.engine = create_engine("%s://%s:%s@%s:%s/%s" % (dialect, \
+            args["user"], args["passwd"], args["host"], args["port"], \
+            args["db"]), pool_recycle = 3600)
+        self.meta = MetaData()
+        self.meta.bind = self.engine
+    
+    
+    def __del__(self):
+        del self.meta
+        del self.engine
+    
+    
+    def createtable(self, tablename, cols):
+        """Create a new instrument table"""
+        columns = [
+            Column("dt_record", DateTime, nullable=False, primary_key=True), \
+            Column("microsec", Integer, default=0, primary_key=True)]
+        for col in cols:
+            columns.append(Column(col, Integer, default=None))
+        tbl = Table(tablename, self.meta, *columns)
+        tbl.create()
+        i = Index('%s_index' % tablename, tbl.c.dt_record)
+        i.create()
+    
+    
+    def droptable(self, tablename):
+        """Drop a table"""
+        tbl = Table(tablename, self.meta, autoload=True)
+        tbl.drop()
+    
+    
+    #def insert(self, table, header, valueslists):
+        #if len(valueslists) == 0:
+            #print "KAKOGO??? Valueslists is empty :("
+        #return
+        #if self.type == "oracle":
+        #columns = ",".join(map(lambda h : '"%s"'%h, header))
+        #placeholders = ",".join(map(lambda i : ":" + str(i+1), range(len(header))))
+        #request = 'INSERT INTO smdc."%s"('%table + columns + ') VALUES (to_date(:1, \'yyyy-mm-dd hh24:mi:ss\'),' + placeholders[3:] + ')'
+        #self.cursor.prepare(request)
+        #try:
+            #self.cursor.executemany(None, valueslists)
+            #self.conn.commit()
+        #except cx_Oracle.Error, strerror:
+            #print strerror[:-1]
+        #elif self.type == "mysql":
+        #columns = ",".join(header)
+        #placeholders = ("%s,"*len(header))[:-1]
+        #trying = 0
+        #try:
+            #maxRows = self.maxAllowedPacked / len(" ".join(map(lambda v : str(v), valueslists[0]))) / 2
+            #i = 0
+            #while i < len(valueslists):
+            #self.cursor.executemany("insert into `" + table + "` ("+columns+") values ("+placeholders+")", valueslists[i:i+maxRows])
+            #i += maxRows
+        #except MySQLError, strerror:
+            #print "MySQLError :", strerror, "trying ..."
+            #trying = 1
+        #if trying:
+            #print "Trying row by row... (", len(valueslists), ") rows" 
+            #for i in range(len(valueslists)):
+            #try:
+                #self.cursor.execute(("insert into `" + table + "` ("+columns+") values ("+placeholders+");")%valueslists[i])
+            #except MySQLError, strerror:
+                #print "MySQLError :", strerror
+            #except:
+                #pass
+        #return 0
 
---
+++ trunk/setup.py
@@ -0,0 +1,6 @@
+from distutils.core import setup
+
+setup(name = 'pysatel',
+    version = '0.1',
+    py_modules = ['foo'],
+    scripts=['pysatel_admin'])
